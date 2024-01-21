422b2410f0a2f09704f4129187f68aecf7b763f5
Priyanshu Kumar Pandey <88549100+rashup198@users.noreply.github.com>
2024-01-21 18:33:33 +0530

    Add user routes and middlewares

--- guide/index.js
+++ guide/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
 require("dotenv").config
-const database = require("./Config/database")
+const database = require("./Config/database");
+const cookieParser = require('cookie-parser');
 
 const PORT = process.env.PORT ||4000
 
+// import the routes
+const userRoutes = require("./Routes/User");
+
 // connecting the database
 database.connect();
+
+// middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+// routes
+app.use("/api/v1/auth", userRoutes);
 
 app.get('/', (req,res)=>{
     res.send("Wellcome to the study guide server!")
