3c258f2c64eb24909ac0d83aeb58fa91c8aac8d9
Priyanshu Kumar Pandey <88549100+rashup198@users.noreply.github.com>
2024-01-21 18:03:31 +0530

    Add login controller to authenticate users

--- guide/Controllers/Auth.js
+++ guide/Controllers/Auth.js
@@ -91,3 +91,66 @@ exports.signup = async (req,res)=>{
         })
     }
 }
+
+/// login controller to login a user
+
+exports.login = async (req, res) => {
+
+    try {
+        // destructuring the request body
+        const {
+            email,
+            password,
+        } = req.body;
+
+        // checking if all details are entered
+
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "Please enter all details",
+            })
+        }
+
+        // find the user with the email
+        const user = await User.findOne({ email }).populate('additionalDetails');
+        if (!user) {
+            return res.status(400).json({
+                message: "User Is not registered with this email",
+            })
+        }
+
+        // generate a token and compare the password
+        if(await bcrypt.compare(password,user.password)){
+            const token = jwt.sign({email:user.email, id:user._id},process.env.JWT_SECRET,{expiresIn:"24h"})
+            
+            // save the token in the database
+            user.token = token;
+            user.password = undefined;
+
+            // set the cookie in the browser
+
+            const options={
+                expires:new Date(Date.now()+ 24*60*60*1000),
+                httpOnly:true,
+            }
+
+            res.cookie("token", token, options).status(200).json({
+                success: true,
+                token,
+                user,
+                message: `User Login Success`,
+              })
+        } else {
+            return res.status(400).json({
+                message: "Please enter correct password",
+            })
+        }
+    
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "There is some error logging in the user please try again later",
+        })
+    }
+}
